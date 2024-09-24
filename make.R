7b5fb48bbba4e6bc7171efd9505ca87af3ba5260
clemdelrevo <clement.delamare@live.fr>
2024-09-24 17:55:50 +0300

    create project structure

---
+++ make.R
@@ -0,0 +1,13 @@
+# renv
+# renv::init()
+# renv::install("usethis")
+# usethis::use_description(check_name = FALSE)
+# renv::install() ; renv::snapshot() ; renv::status()
+
+# project structure
+# reps <- c("R", "assets", "main")
+# lapply(reps, dir.create, showWarnings = FALSE)
+# dir.create("assets/images", showWarnings = FALSE)
+
+# render
+quarto::quarto_render()
